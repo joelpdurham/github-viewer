a94c751515df42a4d6d23f197bf16067197dd9bc
joelpdurham <joeldur@gmail.com>
2020-02-15 13:04:43 -0800

    user page has required info

--- src/components/Users.js
+++ src/components/Users.js
@@ -9,8 +9,11 @@ export const User = () => {
     <>
       <h1>{user.name}</h1>
       <img src={user.avatar_url} />
+      <a href={user.html_url} target='blank' >Visit</a>
       <p>Location: {user.location} - Repos: {user.public_repos}</p>
       <p>{user.bio}</p>
+      <p>Following: {user.following}</p>
+      <p>Followers: {user.followers}</p>
     </>
   );
 };
