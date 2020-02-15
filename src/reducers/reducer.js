e746a090280fd93948e5054042e992291b8d35b0
joelpdurham <joeldur@gmail.com>
2020-02-15 12:20:56 -0800

    wrote failing reducer test

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,1 +1,10 @@
+import { FETCH_USER } from "../actions/actions";
 
+export const reducer = (state = intialState, action) => {
+  switch(action.type) {
+    case FETCH_USER:
+      return { ...state, userInfo: action.payload };
+    default: 
+      return state;
+  }
+}
